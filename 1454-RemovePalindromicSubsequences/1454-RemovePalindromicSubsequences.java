af25c3a64a9133e5232d05060227415261160d8b
Kd-2006 <24244016@drngpit.ac.in>
2026-07-14 15:58:15 +0530

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.java
@@ -0,0 +1,9 @@
+// Last updated: 7/14/2026, 3:59:32 PM
+class Solution {
+    public int removePalindromeSub(String S) {
+        if (S.length() == 0) return 0;
+        for (int i = 0, j = S.length() - 1; i < j; i++, j--)
+            if (S.charAt(i) != S.charAt(j)) return 2;
+        return 1;
+    }
+}
