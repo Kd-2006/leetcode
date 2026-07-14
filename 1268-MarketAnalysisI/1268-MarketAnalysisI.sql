ca805cab1ae1cb482e05bc505088c2eea5bd5869
Kd-2006 <24244016@drngpit.ac.in>
2026-07-14 15:58:53 +0530

    Create 1268-MarketAnalysisI

---
+++ 1268-MarketAnalysisI/1268-MarketAnalysisI.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/14/2026, 4:00:10 PM
+SELECT 
+    u.user_id AS buyer_id,
+    u.join_date,
+    COUNT(o.order_id) AS orders_in_2019
+FROM Users u
+LEFT JOIN Orders o
+    ON u.user_id = o.buyer_id
+    AND YEAR(o.order_date) = 2019
+GROUP BY u.user_id, u.join_date;
