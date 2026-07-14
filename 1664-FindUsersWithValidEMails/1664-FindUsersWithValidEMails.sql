cf8057611566741ab14da4bb8e3bd373ca6ade7f
Kd-2006 <24244016@drngpit.ac.in>
2026-07-14 15:57:46 +0530

    Create 1664-FindUsersWithValidEMails

---
+++ 1664-FindUsersWithValidEMails/1664-FindUsersWithValidEMails.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/14/2026, 3:59:04 PM
+SELECT 
+    user_id, 
+    name, 
+    mail
+FROM 
+    users
+WHERE 
+    mail REGEXP '^[a-zA-Z][a-zA-Z0-9_.-]*@leetcode\\.com$' 
+    AND mail LIKE BINARY '%@leetcode.com';
