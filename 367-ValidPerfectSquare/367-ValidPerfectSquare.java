645e875ad30b929eca1618ce25471c81b9b793e1
Kd-2006 <24244016@drngpit.ac.in>
2026-07-17 13:23:07 +0530

    File updated at 7/17/2026, 1:24:26 PM

--- 367-ValidPerfectSquare/367-ValidPerfectSquare.java
+++ 367-ValidPerfectSquare/367-ValidPerfectSquare.java
@@ -1,25 +1,22 @@
-// Last updated: 7/17/2026, 1:23:09 PM
+// Last updated: 7/17/2026, 1:24:26 PM
 1class Solution {
-2    public List<String> readBinaryWatch(int turnedOn) {
-3        List<String> result = new ArrayList<>();
-4
-5        for (int hour = 0; hour < 12; hour++) {
-6            for (int minute = 0; minute < 60; minute++) {
-7                if (countBits(hour) + countBits(minute) == turnedOn) {
-8                    result.add(hour + ":" + String.format("%02d", minute));
-9                }
-10            }
-11        }
-12
-13        return result;
-14    }
-15
-16    private int countBits(int n) {
-17        int count = 0;
-18        while (n > 0) {
-19            count += n & 1;
-20            n >>= 1;
-21        }
-22        return count;
-23    }
-24}
+2    public String toHex(int num) {
+3        if (num == 0) {
+4            return "0";
+5        }
+6
+7        char[] hex = {
+8            '0','1','2','3','4','5','6','7',
+9            '8','9','a','b','c','d','e','f'
+10        };
+11
+12        StringBuilder sb = new StringBuilder();
+13
+14        while (num != 0) {
+15            sb.append(hex[num & 15]);  
+16            num >>>= 4;                 
+17        }
+18
+19        return sb.reverse().toString();
+20    }
+21}
