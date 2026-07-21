7170eaa8d4b38100dede3944b539af446e5b9969
Kd-2006 <24244016@drngpit.ac.in>
2026-07-21 23:23:56 +0530

    File updated at 7/21/2026, 11:25:18 PM

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.java
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -1,19 +1,33 @@
-// Last updated: 7/21/2026, 11:24:43 PM
+// Last updated: 7/21/2026, 11:25:18 PM
 1class Solution {
-2    public int jump(int[] nums) {
-3        int jumps = 0;
-4        int currentEnd = 0;
-5        int farthest = 0;
-6
-7        for (int i = 0; i < nums.length - 1; i++) {
-8            farthest = Math.max(farthest, i + nums[i]);
-9
-10            if (i == currentEnd) {
-11                jumps++;
-12                currentEnd = farthest;
-13            }
-14        }
-15
-16        return jumps;
-17    }
-18}
+2    public List<List<Integer>> permute(int[] nums) {
+3        List<List<Integer>> res = new ArrayList<>();
+4        if (nums.length == 1) {
+5            List<Integer> singleList = new ArrayList<>();
+6            singleList.add(nums[0]);
+7            res.add(singleList);
+8            return res;
+9        }
+10
+11        for (int i = 0; i < nums.length; i++) {
+12            int n = nums[i];
+13            int[] remainingNums = new int[nums.length - 1];
+14            int index = 0;
+15            for (int j = 0; j < nums.length; j++) {
+16                if (j != i) {
+17                    remainingNums[index] = nums[j];
+18                    index++;
+19                }
+20            }
+21            
+22            List<List<Integer>> perms = permute(remainingNums);
+23            for (List<Integer> p : perms) {
+24                p.add(n);
+25            }
+26            
+27            res.addAll(perms);
+28        }
+29        
+30        return res;        
+31    }
+32}
