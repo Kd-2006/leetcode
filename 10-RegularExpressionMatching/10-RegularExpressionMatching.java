86d5d0a29f00243cf9413870417ead9a6c526a93
Kd-2006 <24244016@drngpit.ac.in>
2026-07-21 23:26:29 +0530

    File updated at 7/21/2026, 11:27:50 PM

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.java
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -1,29 +1,14 @@
-// Last updated: 7/21/2026, 11:27:17 PM
+// Last updated: 7/21/2026, 11:27:50 PM
 1class Solution {
-2    public int thirdMax(int[] nums) {
-3        long max = Long.MIN_VALUE;
-4        long secondMax = Long.MIN_VALUE;
-5        long thirdMax = Long.MIN_VALUE;
-6
-7        for (int num : nums) {
-8            long val = num;
-9
-10            if (val == max || val == secondMax || val == thirdMax) {
-11                continue;
-12            }
-13
-14            if (val > max) {
-15                thirdMax = secondMax;
-16                secondMax = max;
-17                max = val;
-18            } else if (val > secondMax) {
-19                thirdMax = secondMax;
-20                secondMax = val;
-21            } else if (val > thirdMax) {
-22                thirdMax = val;
-23            }
-24        }
-25
-26        return thirdMax == Long.MIN_VALUE ? (int) max : (int) thirdMax;
-27    }
-28}
+2    public int arrangeCoins(int n) {
+3        long left = 1, right = n;
+4        while (left <= right) {
+5            long mid = left + (right - left) / 2;
+6            long coins_needed = mid * (mid + 1) / 2;
+7            if (coins_needed == n) return (int) mid;
+8            else if (coins_needed < n) left = mid + 1;
+9            else right = mid - 1;
+10        }
+11        return (int) right;
+12    }
+13}
