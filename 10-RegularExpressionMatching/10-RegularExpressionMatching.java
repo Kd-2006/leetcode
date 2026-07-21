c602690ad3ff1e916ab51b7c2a285c4341f09f40
Kd-2006 <24244016@drngpit.ac.in>
2026-07-21 23:24:46 +0530

    File updated at 7/21/2026, 11:26:07 PM

--- 10-RegularExpressionMatching/10-RegularExpressionMatching.java
+++ 10-RegularExpressionMatching/10-RegularExpressionMatching.java
@@ -1,33 +1,16 @@
-// Last updated: 7/21/2026, 11:25:18 PM
+// Last updated: 7/21/2026, 11:26:07 PM
 1class Solution {
-2    public List<List<Integer>> permute(int[] nums) {
-3        List<List<Integer>> res = new ArrayList<>();
-4        if (nums.length == 1) {
-5            List<Integer> singleList = new ArrayList<>();
-6            singleList.add(nums[0]);
-7            res.add(singleList);
-8            return res;
-9        }
-10
-11        for (int i = 0; i < nums.length; i++) {
-12            int n = nums[i];
-13            int[] remainingNums = new int[nums.length - 1];
-14            int index = 0;
-15            for (int j = 0; j < nums.length; j++) {
-16                if (j != i) {
-17                    remainingNums[index] = nums[j];
-18                    index++;
-19                }
-20            }
-21            
-22            List<List<Integer>> perms = permute(remainingNums);
-23            for (List<Integer> p : perms) {
-24                p.add(n);
-25            }
-26            
-27            res.addAll(perms);
-28        }
-29        
-30        return res;        
-31    }
-32}
+2    public boolean isSubsequence(String s, String t) {
+3        int sp = 0;
+4        int tp = 0;
+5
+6        while (sp < s.length() && tp < t.length()) {
+7            if (s.charAt(sp) == t.charAt(tp)) {
+8                sp++;
+9            }
+10            tp++;
+11        }
+12
+13        return sp == s.length();        
+14    }
+15}
