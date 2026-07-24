ac7bd8933b2918300621c4c7dba1633f2a6062b7
Kd-2006 <24244016@drngpit.ac.in>
2026-07-24 20:01:44 +0530

    File updated at 7/24/2026, 8:03:08 PM

--- 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
+++ 482-LicenseKeyFormatting/482-LicenseKeyFormatting.java
@@ -1,25 +1,18 @@
-// Last updated: 7/24/2026, 8:02:03 PM
+// Last updated: 7/24/2026, 8:03:08 PM
 1class Solution {
-2    public String licenseKeyFormatting(String s, int k) {
-3        StringBuilder result = new StringBuilder();
-4        int count = 0;
+2    public int findMaxConsecutiveOnes(int[] nums) {
+3        int count = 0;
+4        int max = 0;
 5
-6        for (int i = s.length() - 1; i >= 0; i--) {
-7            char ch = s.charAt(i);
-8
-9            if (ch == '-') {
-10                continue;
-11            }
-12
-13            if (count == k) {
-14                result.append('-');
-15                count = 0;
-16            }
-17
-18            result.append(Character.toUpperCase(ch));
-19            count++;
-20        }
-21
-22        return result.reverse().toString();
-23    }
-24}
+6        for (int num : nums) {
+7            if (num == 1) {
+8                count++;
+9                max = Math.max(max, count);
+10            } else {
+11                count = 0;
+12            }
+13        }
+14
+15        return max;
+16    }
+17}
